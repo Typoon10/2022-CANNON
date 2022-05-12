e63620b4af44d1e5a8a50d9b940afc03f6f4f384
Titans2620 <starobotics2620@gmail.com>
2022-05-12 19:24:34 -0400

    Lawrencio
    Created and Defined Subsystems and defined buttons.

--- 2022-CANNON/src/main/java/frc/robot/RobotContainer.java
+++ 2022-CANNON/src/main/java/frc/robot/RobotContainer.java
@@ -6,9 +6,13 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.XboxController;
-import frc.robot.commands.ExampleCommand;
-import frc.robot.subsystems.ExampleSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.button.Trigger;
+import frc.robot.subsystems.AimSubsystem;
+import frc.robot.subsystems.CompressorSubsystem;
+import frc.robot.subsystems.DriveSubsystem;
+import frc.robot.subsystems.LEDSubsystem;
+import frc.robot.subsystems.ShootSubsystem;
 
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a
@@ -18,9 +22,13 @@ import edu.wpi.first.wpilibj2.command.Command;
  */
 public class RobotContainer {
   // The robot's subsystems and commands are defined here...
-  private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
+    AimSubsystem aimSubsystem = new AimSubsystem();
+    CompressorSubsystem compressorSubsystem = new CompressorSubsystem();
+    LEDSubsystem ledSubsystem = new LEDSubsystem();
+    ShootSubsystem shootSubsystem = new ShootSubsystem();
+    DriveSubsystem driveSubsystem = new DriveSubsystem();
 
-  private final ExampleCommand m_autoCommand = new ExampleCommand(m_exampleSubsystem);
+    XboxController controller = new XboxController(0);
 
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
@@ -34,15 +42,20 @@ public class RobotContainer {
    * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing it to a {@link
    * edu.wpi.first.wpilibj2.command.button.JoystickButton}.
    */
-  private void configureButtonBindings() {}
+  private void configureButtonBindings() {
+
+    
+  }
 
   /**
    * Use this to pass the autonomous command to the main {@link Robot} class.
    *
    * @return the command to run in autonomous
    */
+  /*
   public Command getAutonomousCommand() {
     // An ExampleCommand will run in autonomous
     return m_autoCommand;
   }
+  */
 }
